2f0a3fd88a6fec545bf4df5b8c06b37474b1fb0a
Changsu <janghyang_@naver.com>
2023-12-05 17:30:11 +0900

    ㅇㄷ

--- UI/app.py
+++ UI/app.py
@@ -1,6 +1,5 @@
 """
-Simple app to upload an image via a web form 
-and view the inference results on the image in the browser.
+Simple app to upload an image via a web form and view the inference results on the image in the browser.
 """
 import argparse
 import io
@@ -22,75 +21,197 @@ model = torch.hub.load(
         )#.autoshape()  # force_reload = recache latest code
 #'''
 # Load Custom Model
-model = torch.hub.load("ultralytics/yolov5", "custom", path = "./best_damage.pt", force_reload=True)
+# model = torch.hub.load("ultralytics/yolov5", "custom", path = "./best_damage.pt", force_reload=True)
 
 # Set Model Settings
-model.eval()
+
 model.conf = 0.6  # confidence threshold (0-1)
-model.iou = 0.45  # NMS IoU threshold (0-1) 
+model.iou = 0.45  # NMS IoU threshold (0-1)
 
 from io import BytesIO
 
+#  This is copied from to test the code is working or not 
+# ======================================================
+# ======================================================
+import argparse
+import csv
+import os
+import platform
+import sys
+from pathlib import Path
+
+import torch
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from ultralytics.utils.plotting import Annotator, colors, save_one_box
+
+from models.common import DetectMultiBackend
+from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                           increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+from utils.torch_utils import select_device, smart_inference_mode
+
+# This is copied from to test the code is working or not 
+# ======================================================
+# ======================================================
+
+@smart_inference_mode()
 def gen():
-    cap=cv2.VideoCapture("http://192.168.0.4:8080/?action=stream/")
-    # Read until video is completed
-    while(cap.isOpened()):
-        
-        # Capture frame-by-fram ## read the camera frame
-        success, frame = cap.read()
-        if success == True:
+    weights=ROOT / 'best.pt'  # model path or triton URL
+    source= "https://www.youtube.com/watch?v=5JYZ0FyS39Y"
+    data=ROOT / 'data/coco128.yaml'  # dataset.yaml path
+    imgsz=(640, 640)  # inference size (height, width)
+    conf_thres=0.25  # confidence threshold
+    iou_thres=0.45  # NMS IOU threshold
+    max_det=1000  # maximum detections per image
+    device=''  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+    view_img=False  # show results
+    save_txt=False  # save results to *.txt
+    save_csv=False  # save results in CSV format
+    save_conf=False  # save confidences in --save-txt labels
+    save_crop=False  # save cropped prediction boxes
+    nosave=False  # do not save images/videos
+    classes=None  # filter by class: --class 0, or --class 0 2 37
+029*-8*+++++    agnostic_nms=False  # class-agnostic NMS
+    augment=False  # augmented inference
+    visualize=False  # visualize features
+    update=False  # update all models
+    project=ROOT / 'runs/detect'  # save results to project/name
+    name='exp'  # save results to project/name
+    exist_ok=False,  # existing project/name ok, do not increment
+    line_thickness=3  # bounding box thickness (pixels)
+    hide_labels=False  # hide labelsde
+    hide_conf=False  # hide confidences
+    half=False  # use FP16 half-precision inference
+    dnn=False  # use OpenCV DNN for ONNX inference
+    vid_stride=1  # video frame-rate stride
+    source = str(source)
+    save_img = not nosave and not source.endswith('.txt')  # save inference images
+    is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
+    is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
+    webcam = source.isnumeric() or source.endswith('.streams') or (is_url and not is_file)
+    screenshot = source.lower().startswith('screen')
+    if is_url and is_file:
+        source = check_file(source)  # download
+    
 
-            ret,buffer=cv2.imencode('.jpg',frame)
+
+    # Directories
+    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Load model
+    device = select_device(device)
+    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Dataloader
+    bs = 1  # batch_size
+    if webcam:
+        view_img = check_imshow(warn=True)
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+        bs = len(dataset)
+    elif screenshot:
+        dataset = LoadScreenshots(source, img_size=imgsz, stride=stride, auto=pt)
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
+    vid_path, vid_writer = [None] * bs, [None] * bs
+
+    # Run inference
+    model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
+    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+    for path, im, im0s, vid_cap, s in dataset:
+        with dt[0]:
+            im = torch.from_numpy(im).to(model.device)
+            im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+            im /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(im.shape) == 3:
+                im = im[None]  # expand for batch dim
+
+        # Inference
+        with dt[1]:
+            visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+            pred = model(im, augment=augment, visualize=visualize)
+
+        # NMS
+        with dt[2]:
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+
+        # Second-stage classifier (optional)
+        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
+
+        # Define the path for the CSV file
+        csv_path = save_dir / 'predictions.csv'
+
+        # Create or append to the CSV file
+        def write_to_csv(image_name, prediction, confidence):
+            data = {'Image Name': image_name, 'Prediction': prediction, 'Confidence': confidence}
+            with open(csv_path, mode='a', newline='') as f:
+                writer = csv.DictWriter(f, fieldnames=data.keys())
+                if not csv_path.is_file():
+                    writer.writeheader()
+                writer.writerow(data)
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            print("Inside the loop")
+            seen += 1
+            if webcam:  # batch_size >= 1
+                p, im0, frame = path[i], im0s[i].copy(), dataset.count
+                s += f'{i}: '
+            else:
+                p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # im.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
+            s += '%gx%g ' % im.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            imc = im0.copy() if save_crop else im0  # for save_crop
+            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, 5].unique():
+                    n = (det[:, 5] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    c = int(cls)  # integer class
+                    label = names[c] if hide_conf else f'{names[c]}'
+                    confidence = float(conf)
+                    confidence_str = f'{confidence:.2f}'
+
+                    if save_csv:
+                        write_to_csv(p.name, label, confidence_str)
+
+                    if save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                        with open(f'{txt_path}.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    if save_img or save_crop or view_img:  # Add bbox to image
+                        c = int(cls)  # integer class
+                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                        annotator.box_label(xyxy, label, color=colors(c, True))
+                    if save_crop:
+                        save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+
+            # Stream results
+            im0 = annotator.result()
+            ret,buffer=cv2.imencode('.jpg',im0)
             frame=buffer.tobytes()
-            
-            #print(type(frame))
-
-            img = Image.open(io.BytesIO(frame))
-            results = model(img, size=640)
-            #print(results)
-            #print(results.pandas().xyxy[0])
-            #results.render()  # updates results.imgs with boxes and labels
-            results.print()  # print results to screen
-            #results.show() 
-            #print(results.imgs)
-            #print(type(img))
-            #print(results)
-            #plt.imshow(np.squeeze(results.render()))
-            #print(type(img))
-            #print(img.mode)
-            
-            #convert remove single-dimensional entries from the sh ape of an array
-            img = np.squeeze(results.render()) #RGB
-            # read image as BGR
-            img_BGR = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #BGR
-
-            #print(type(img))
-            #print(img.shape)
-            #frame = img
-            #ret,buffer=cv2.imencode('.jpg',img)
-            #frame=buffer.tobytes()
-            #print(type(frame))
-            #for img in results.imgs:
-                #img = Image.fromarray(img)
-            #ret,img=cv2.imencode('.jpg',img)
-            #img=img.tobytes()
-
-            #encode output image to bytes
-            #img = cv2.imencode('.jpg', img)[1].tobytes()
-            #print(type(img))
-        else:
-            break
-        #print(cv2.imencode('.jpg', img)[1])
-
-        #print(b)
-        #frame = img_byte_arr
-
-        # Encode BGR image to bytes so that cv2 will convert to RGB
-        frame = cv2.imencode('.jpg', img_BGR)[1].tobytes()
-        #print(frame)
-        
-        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+            yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def home():
@@ -141,7 +262,7 @@ def predict():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
-    parser.add_argument("--port", default=5000, type=int, help="port number")
+    parser.add_argument("--port", default=9090, type=int, help="port number")
     args = parser.parse_args()
     '''
     model = torch.hub.load(
@@ -150,7 +271,3 @@ if __name__ == "__main__":
     model.eval()
     '''
     app.run(host="0.0.0.0", port=args.port, threaded=True)  # debug=True causes Restarting with stat
-
-# Docker Shortcuts
-# docker build --tag yolov5 .
-# docker run --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --decdvice="/dev/video0:/dev/video0" yolov5
